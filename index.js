ed2b8e50db201f0f96b574b4f6928360e8ec286d
Justin Cefai <justincefai@Justins-MacBook-Pro.local>
2024-09-13 13:22:16 -0700

    navigation updates - mobile nav added

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const contactModal = document.getElementById("contactModal");
 const contactBody = document.getElementById("contactBody");
 const openContact = document.getElementById("openContact");
 const closeContact = document.getElementById("closeContact");
+
 const closeMobileNavBtn = document.getElementById("closeMobileNav");
 
 const workModal = document.getElementById("workModal");
 const workBody = document.getElementById("workBody");
 const openWork = document.getElementById("openWork");
 const closeWork = document.getElementById("closeWork");
+
+
+const openWorkDesktop = document.getElementById("openWorkDesktop");
+const closeWorkDesktop = document.getElementById("closeWorkDesktop");
+const openContactDesktop = document.getElementById("openContactDesktop");
+const closeContactDesktop = document.getElementById("closeContactDesktop");
+const openAboutDesktop = document.getElementById("openAboutDesktop");
+const closeAboutDesktop = document.getElementById("closeAboutDesktop");
 
 const showProjects = document.getElementById("showProjects");
 const showTestimonials = document.getElementById("showTestimonials");
@@ -26,16 +35,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function closeMobileNav() {
     mobileNavList.classList.remove("visible");
-    mobileSvgTop.classList.remove("close-top");
-    mobileSvgBottom.classList.remove("close-bottom");
-    mobileNav.classList.remove("close-nav");
   }
 
   mobileNavButton.onclick = function () {
     mobileNavList.classList.add("visible");
-    mobileSvgTop.classList.add("close-top");
-    mobileSvgBottom.classList.add("close-bottom");
-    mobileNav.classList.add("close-nav");
   }
 
   closeMobileNavBtn.onclick = function() {
@@ -44,6 +47,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Open About Modal
   openAbout.onclick = function (event) {
+    event.preventDefault();
+    aboutModal.classList.add("show");
+    document.body.classList.add('modal-open');
+    wrapperMain.classList.add('modal-open');
+
+    fetch("/about/about.html")
+      .then((response) => response.text())
+      .then((data) => {
+        aboutBody.innerHTML = data;
+      })
+      .catch((error) => console.error("Error loading about page:", error));
+  };
+
+  openAboutDesktop.onclick = function (event) {
     event.preventDefault();
     aboutModal.classList.add("show");
     document.body.classList.add('modal-open');
@@ -79,6 +96,20 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch((error) => console.error("Error loading contact page:", error));
   };
 
+  openContactDesktop.onclick = function (event) {
+    event.preventDefault();
+    contactModal.classList.add("show");
+    document.body.classList.add('modal-open');
+    wrapperMain.classList.add('modal-open');
+
+    fetch("/contact/contact.html")
+      .then((response) => response.text())
+      .then((data) => {
+        contactBody.innerHTML = data;
+      })
+      .catch((error) => console.error("Error loading contact page:", error));
+  };
+
   closeContact.onclick = function () {
     contactModal.classList.remove("show");
     document.body.classList.remove('modal-open');
@@ -88,6 +119,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Open Work Modal
   openWork.onclick = function (event) {
+    event.preventDefault();
+    workModal.classList.add("show");
+    document.body.classList.add('modal-open');
+    wrapperMain.classList.add('modal-open');
+
+    fetch("/work/workV2.html")
+      .then((response) => response.text())
+      .then((data) => {
+        workBody.innerHTML = data;
+
+        const projects = document.getElementById("projects");
+        const testimonials = document.getElementById("testimonials");
+        const projectsNav = document.getElementById("projectsNav");
+        const testimonialsNav = document.getElementById("testimonialsNav");
+
+        projects.classList.add("show"); 
+        testimonials.classList.remove("show");
+        projectsNav.classList.add("active");
+
+        projectsNav.onclick = function (event) {
+          event.preventDefault();
+          projects.classList.add("show");
+          testimonials.classList.remove("show");
+          testimonialsNav.classList.remove("active");
+          projectsNav.classList.add("active");
+        };
+
+        testimonialsNav.onclick = function (event) {
+          event.preventDefault();
+          projects.classList.remove("show");
+          testimonials.classList.add("show");
+          testimonialsNav.classList.add("active");
+          projectsNav.classList.remove("active");
+        };
+      })
+      .catch((error) => console.error("Error loading work page:", error));
+  };
+
+  openWorkDesktop.onclick = function (event) {
     event.preventDefault();
     workModal.classList.add("show");
     document.body.classList.add('modal-open');
